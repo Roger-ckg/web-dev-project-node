12816e5f1c80ee18bb5bd0f6cff8ac9fc70e8f0f
luwang <wanglu1207>
2021-12-12 12:36:35 -0800

    node

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(process.env.PORT || 4001);
+// const express = require('express');
+// const app = express();
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods",
+//         "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+// });
+// app.get('/hello', (req, res) => {
+//     res.send('Hello World!');
+// });
+//
+// app.listen(process.env.PORT || 4001);
