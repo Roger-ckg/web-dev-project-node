9cee604384d6aeb99a6d767410927e8aa7803d9b
Roger <Roger@mac.com>
2021-12-13 14:24:32 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-// const express = require('express');
-// const app = express();
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header("Access-Control-Allow-Methods",
-//         "GET, POST, PUT, DELETE, OPTIONS");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
-// app.get('/hello', (req, res) => {
-//     res.send('Hello World!');
-// });
-//
-// app.listen(process.env.PORT || 4001);
-=======
 const express = require('express');
 const app = express();
 app.use(function(req, res, next) {
@@ -37,4 +19,3 @@ const MONGODB_URL = 'mongodb+srv://web-dev-project:web-dev-project@cluster0.zboy
 mongoose.connect(MONGODB_URL);
 
 app.listen(process.env.PORT || 4001);
->>>>>>> 9d55b4c0f3de393cec31c20513f4f34d3fb050a0
